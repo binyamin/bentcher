47ab7392d06de2ec38b601e7d204285ba7ae3c87
Binyamin Aron Green <binyamingreen@protonmail.com>
2023-08-09 15:35:31 -0400

    Update dependencies

--- _config.js
+++ _config.js
@@ -3,7 +3,7 @@ import postcss from 'lume/plugins/postcss.ts';
 import esbuild from 'lume/plugins/esbuild.ts';
 import imagick from 'lume/plugins/imagick.ts';
 
-import clean_css from 'https://deno.land/x/lume_cleancss@v0.2.0/mod.ts';
+import clean_css from 'https://deno.land/x/lume_cleancss@v0.2.1/mod.ts';
 
 const site = lume({
 	src: 'src',
@@ -14,15 +14,17 @@ site.filter('log', console.log);
 
 site.addEventListener('afterBuild', async (_event) => {
 	const fontFiles = [
-		'files/noto-serif-hebrew-hebrew-variable-wghtOnly-normal.woff2',
-		'variable.css',
+		'files/noto-serif-hebrew-hebrew-wght-normal.woff2',
+		'files/noto-serif-hebrew-latin-wght-normal.woff2',
+		'files/noto-serif-hebrew-latin-ext-wght-normal.woff2',
+		'wght.css',
 	];
 	await Deno.mkdir(site.dest('fonts/noto-serif-hebrew/files'), {
 		recursive: true,
 	});
 	for await (const file of fontFiles) {
 		await Deno.copyFile(
-			`${Deno.cwd()}/node_modules/@fontsource/noto-serif-hebrew/${file}`,
+			`${Deno.cwd()}/node_modules/@fontsource-variable/noto-serif-hebrew/${file}`,
 			site.dest('fonts/noto-serif-hebrew/' + file),
 		);
 	}
